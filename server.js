4dcf386a1366432827556990b5984ba31e2191a0
Susan Holland <segh@fastmail.com>
2020-11-12 19:16:25 -0500

    Used bodyParser, routes, and connected to database with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Import dependencies
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +13,16 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/dist"));
 }
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.use(routes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workDash", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
 
 // Start the API server
 app.listen(PORT, function() {
