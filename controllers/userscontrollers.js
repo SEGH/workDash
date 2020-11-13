b74e4583a3e957697bd80ce551d402ba47f7d068
Susan Holland <segh@fastmail.com>
2020-11-12 19:31:29 -0500

    Used bcrypt to hash password before user is created

--- controllers/userscontrollers.js
+++ controllers/userscontrollers.js
@@ -1,11 +1,14 @@
 const db = require("../models");
+const bcrypt = require("bcrypt");
 
 module.exports = {
     create: function (req, res) {
+        // Using bcrypt to hash password before it is stored in the DB
+        const hash = bcrypt.hashSync(req.body.password, 12);
         db.User
             .create({
                 username: req.body.username,
-                password: req.body.password
+                password: hash
             })
             .then(user => res.json(user))
             .catch(err => res.status(422).json(err));
