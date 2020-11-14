4bd0927c4a2d0a381dd296eb7a94cdf820ebfb74
Susan Holland <segh@fastmail.com>
2020-11-14 12:48:30 -0500

    Added authentication service

---
+++ client/src/app/authentication.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface UserData {
+  username: string,
+  password: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  // loginUser(user) {
+  //   return this.http.post(`http://localhost:3001/api/users/login`, user).toPromise();
+  // }
+  private request(
+    method: "post",
+    type: "login",
+    user: UserData
+  ): Observable<any> {
+    return this.http.post(`http://localhost:3001/api/users/login`, user);
+  }
+
+  public loginUser(user: UserData): Observable<any> {
+    return this.request("post", "login", user);
+  }
+}
