d51a21f8ecc62f419bb04f07e61f311b97410652
Susan Holland <segh@fastmail.com>
2020-11-27 17:45:54 -0500

    Added onInit to check for user, isLoggedIn boolean, and logout method

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from "./authentication.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
+
+  isLoggedIn: boolean;
+
+  ngOnInit() {
+    this.authService.getUser().subscribe(
+      user => {
+        if (user) {
+          this.isLoggedIn = true;
+        } else {
+          this.isLoggedIn = false;
+        }
+      }
+    )
+  }
+
+  logout() {
+    this.authService.logout().subscribe(
+      () => {
+        console.log("logged out");
+        this.isLoggedIn = false;
+        this.router.navigateByUrl("/");
+      }
+    )
+  }
 }
