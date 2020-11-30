ac20f8b9fb85576a8808e3755e2d43c8aeb9346e
Susan Holland <segh@fastmail.com>
2020-11-30 17:30:46 -0500

    Added eventRoutes to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const eventRoutes = require("./events");
 
 router.use("/users", userRoutes);
+router.use("/events", eventRoutes);
 
 module.exports = router;
