84f68e7e21bdf5520e8e901eb54d32e5ac44146a
Susan Holland <segh@fastmail.com>
2020-12-02 18:33:15 -0500

    Added create method for events

--- controllers/eventscontrollers.js
+++ controllers/eventscontrollers.js
@@ -5,6 +5,27 @@ module.exports = {
     create: function (req, res) {
         console.log(req.body);
         console.log(req.params);
-        res.send(req.status);
+        db.Event
+            .create({
+                title: req.body.title,
+                start: req.body.start,
+                end: req.body.end,
+                color: req.body.color,
+                actions: req.body.actions,
+                draggable: req.body.draggable,
+                resizeable: req.body.resizeable
+            })
+            .then(event => {
+                console.log(event);
+                db.User.findOneAndUpdate({ _id: mongoose.Types.ObjectId(req.params.userId) }, { $push: { events: event }})
+                    .then(user => {
+                        console.log(user);
+                        res.json(user);
+                    })
+                    .catch(err => {
+                        res.status(422).json(err);
+                    });
+            })
+            .catch(err => res.status(422).json(err));
     }
 }
