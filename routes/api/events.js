940fce4f1525a5fb1210511883ec7f9516d3d584
Susan Holland <segh@fastmail.com>
2020-12-02 19:29:56 -0500

    Added get method to events/userId route

--- routes/api/events.js
+++ routes/api/events.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const eventsController = require("../../controllers/eventscontrollers");
 
 router.route("/:userId")
+    .get(eventsController.getAll)
     .post(eventsController.create);
 
 module.exports = router;
