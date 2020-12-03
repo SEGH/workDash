7d6591dcccd67b7e5077efbcab1df308b87d9467
Susan Holland <segh@fastmail.com>
2020-12-03 17:26:34 -0500

    Added getEvents function and modified EventData interface

--- client/src/app/calendar/calendar.service.ts
+++ client/src/app/calendar/calendar.service.ts
@@ -7,11 +7,14 @@ import { CalendarEventAction } from 'angular-calendar';
 export interface EventData {
     title: string,
     start: Date,
-    end: Date,
+    end?: Date,
     color: any,
-    actions: CalendarEventAction[],
-    draggable: boolean,
-    resizeable: {}
+    actions?: CalendarEventAction[],
+    draggable?: boolean,
+    resizable?: {
+      beforeStart?: boolean;
+      afterEnd?: boolean;
+    }
 }
 
 @Injectable({
@@ -23,16 +26,26 @@ export class CalendarService {
     constructor(private http: HttpClient) { }
 
     private request(
-        method: "post",
-        type: "event",
-        event: EventData,
-        userId: string
+        method: "post" | "get",
+        type: "event" | "allEvents",
+        userId: string,
+        event?: EventData,
     ): Observable<any> {
-        return this.http.post(`api/events/${userId}`, event)
+        if (method === "post") {
+          return this.http.post(`api/events/${userId}`, event)
+        } else {
+          return this.http.get(`api/events/5fadd31bdc630d854c5832ea`);
+        }
+
     }
 
     saveEvent(event: EventData, userId): Observable<any>{
         console.log(event);
-        return this.request("post", "event", event, userId);
-    } 
+        return this.request("post", "event", userId, event);
+    }
+
+    getEvents(userId): Observable<any>{
+      console.log(userId);
+      return this.request("get", "allEvents", userId);
+    }
 }
