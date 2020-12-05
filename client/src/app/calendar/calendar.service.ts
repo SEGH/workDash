8c04f43065a0c61289c6b1f765a3b7ce4fbf9456
Susan Holland <segh@fastmail.com>
2020-12-04 19:03:38 -0500

    Added deleteEvent function

--- client/src/app/calendar/calendar.service.ts
+++ client/src/app/calendar/calendar.service.ts
@@ -3,47 +3,51 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from "rxjs";
 
 export interface EventData {
-    title: string,
-    start: Date,
-    end?: Date,
-    color: any,
-    actions?: any,
-    draggable?: boolean,
-    resizable?: {
-      beforeStart?: boolean;
-      afterEnd?: boolean;
-    }
+  title: string,
+  start: Date,
+  end?: Date,
+  color?: any,
+  actions?: any,
+  draggable?: boolean,
+  resizable?: {
+    beforeStart?: boolean;
+    afterEnd?: boolean;
+  }
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class CalendarService {
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    private request(
-        method: "post" | "get",
-        type: "event" | "allEvents",
-        userId: string,
-        event?: EventData,
-    ): Observable<any> {
-        if (method === "post") {
-          return this.http.post(`api/events/${userId}`, event)
-        } else {
-          return this.http.get(`api/events/${userId}`);
-        }
-
+  private request(
+    method: "post" | "get",
+    type: "event" | "allEvents",
+    userId?: string,
+    event?: EventData
+  ): Observable<any> {
+    if (method === "post") {
+      return this.http.post(`api/events/${userId}`, event)
+    } else {
+      return this.http.get(`api/events/${userId}`);
     }
 
-    saveEvent(event: EventData, userId): Observable<any>{
-        console.log(event);
-        return this.request("post", "event", userId, event);
-    }
+  }
 
-    getEvents(userId): Observable<any>{
-      console.log(userId);
-      return this.request("get", "allEvents", userId);
-    }
+  saveEvent(event: EventData, userId): Observable<any> {
+    console.log(event);
+    return this.request("post", "event", userId, event);
+  }
+
+  getEvents(userId): Observable<any> {
+    console.log(userId);
+    return this.request("get", "allEvents", userId);
+  }
+
+  deleteEvent(eventId, userId) {
+    return this.http.delete(`api/events/${userId}/${eventId}`);
+  }
 }
